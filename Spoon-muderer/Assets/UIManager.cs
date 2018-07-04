e74acdf0770f80cbc3e49bfefeb666fbbf04f04e
DESKTOP-5A0ST27\SD <able312@korea.ac.kr>
2018-07-04 18:12:07 +0900

    재화 관련 UI
    - 버튼 클릭 시 재화 추가
    - 상단 좌측부에 현재 재화 표시
    - UIManager 스크립트로 버튼 및 재화 관리

---
+++ Spoon-muderer/Assets/UIManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour {
+
+    private float money;
+    Text MoneyText;
+
+	// Use this for initialization
+	void Start () {
+        money = 0;
+        MoneyText = GameObject.Find("current money").GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (MoneyText != null)
+        {
+            MoneyText.text = "Current: " + getMoney();
+        }
+	}
+
+    public float getMoney()
+    {
+        return money;
+    }
+    
+    public void setMoney(float extra)
+    {
+        money = getMoney() + extra;
+    }
+
+    public void onClickButtonMoneyUP()
+    {
+        setMoney(10);
+        Debug.Log("money + 10, now: " + getMoney());
+    }
+}
